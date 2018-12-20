130516400073d745e7f1d5fa80e04c829cdc9124
Eliot McIntire <eliotmcintire@gmail.com>
2018-12-20 14:16:41 -0800

    addNewCohort and rmNewCohort modifications

--- R/LandR.R
+++ R/LandR.R
@@ -37,13 +37,27 @@ if (getRversion() >= "3.1.0") {
 addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, speciesEcoregion) {
   ## get spp "productivity traits" per ecoregion/present year
   ## calculate maximum biomass per ecoregion, join to new cohort data
+  if (isTRUE(getOption("LandR.assertions"))) {
+    ncd <- copy(newCohortData)
+    CD <- copy(cohortData)
+  }
+
+  # This removes the duplicated pixels within pixelGroup, i.e., the reason we want pixelGroups
+  newCohortData <- unique(newCohortData, by = c("pixelGroup", "speciesCode"))
+
   specieseco_current <- speciesEcoregion[year <= time]
   specieseco_current <- na.omit(specieseco_current)
   specieseco_current <- setkey(specieseco_current[year == max(specieseco_current$year),
                                                   .(speciesCode, maxANPP, maxB, ecoregionGroup)],
                                speciesCode, ecoregionGroup)
+
+  # Note that after the following join, some cohorts will be lost due to lack of parameters in speciesEcoregion
+  #  These need to be modified in pixelGroupMap
+  missingNewCohortData <- newCohortData[!specieseco_current, on = c("speciesCode", "ecoregionGroup")]
   specieseco_current[, maxB_eco := max(maxB), by = ecoregionGroup]
-  newCohortData <- newCohortData[specieseco_current, on = c("speciesCode", "ecoregionGroup"), nomatch = 0]
+  newCohortData <- specieseco_current[newCohortData, on = c("speciesCode", "ecoregionGroup")]
+  # newCohortData <- newCohortData[specieseco_current, on = c("speciesCode", "ecoregionGroup"),
+  #                                nomatch = 0]
   #newCohortData <- setkey(newCohortData, speciesCode, ecoregionGroup)[specieseco_current, nomatch = 0]
   set(newCohortData, NULL, "age", 1)  ## set age to 1
   set(newCohortData, NULL, "sumB", 0)
@@ -55,10 +69,54 @@ addNewCohorts <- function(newCohortData, cohortData, pixelGroupMap, time, specie
 
   newCohortData <- newCohortData[, .(pixelGroup, ecoregionGroup, speciesCode, age, B,
                                      mortality = 0, aNPPAct = 0, sumB = 0)]
+
+  # unique(cohortData, by = c("pixelGroup", "speciesCode", "age"))
   cohortData <- rbindlist(list(cohortData, newCohortData))
-  pixelGroupsLostToDisturbance <- setdiff(cohortData$pixelGroup, getValues(pixelGroupMap))
-  cohortData <- cohortData[!pixelGroup %in% pixelGroupsLostToDisturbance]
   return(cohortData)
+}
+
+
+
+#' Remove missing cohorts from cohortData based on pixelGroupMap
+#'
+#'
+#' @param cohortData a \code{data.table} with columns:
+#'   "pixelGroup", "ecoregionGroup", "speciesCode", "age", "B", "mortality", "aNPPAct", "sumB"
+#' @param pixelGroupMap Raster layer with pixel values equal to a pixel group number that
+#'   correspondsd exactly to ]\code{pixelGroup} column in \code{cohortData}
+#' @param firePixelTable A data.table with 2 columns, \code{pixelIndex} and \code{pixelGroup}.
+#'   This will be used in conjunction
+#'   with cohortData and pixelGroupMap to ensure that everything matches correctly.
+#'
+#' @return
+#' A \code{data.table} with a new, cohortData
+#'
+#' @export
+#' @importFrom data.table rbindlist set setkey
+#' @importFrom raster getValues
+#'
+rmMissingCohorts <- function(cohortData, pixelGroupMap, firePixelTable) {
+  pgmVals <- na.omit(getValues(pixelGroupMap))
+  pgsStillInCDGoneFromPGM <- setdiff(cohortData$pixelGroup, pgmVals)
+
+  # REMOVE lines in cohortData that are no longer in the pixelGroupMap
+  cohortData <- cohortData[!pixelGroup %in% pgsStillInCDGoneFromPGM]
+
+  if (isTRUE(getOption("LandR.assertions"))) {
+    # All pixels in pgsStillInCDGoneFromPGM should have been touched by a fire
+    test1 <- isTRUE(all(pgsStillInCDGoneFromPGM %in% na.omit(firePixelTable$pixelGroup)))
+
+    # There should still be some burned pixel groups that are still on the map, i.e., only some pixels from a PG got burned
+    burnedByPGStillOnMap <- setdiff(na.omit(firePixelTable$pixelGroup), pgsStillInCDGoneFromPGM)
+    test2 <- isTRUE(all(burnedByPGStillOnMap %in% pgmVals))
+
+    test3 <- length(setdiff(cohortData$pixelGroup, pgmVals)) == 0
+
+    if (!isTRUE(all(test1, test2, test3)))
+      stop("cohortData and pixelGroupMap don't match")
+  }
+
+  cohortData
 }
 
 #' Assign light probability
