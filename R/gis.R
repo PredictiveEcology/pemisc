b639780f598023499b5c008e0f384056559f84e5
Alex Chubaty <alex.chubaty@gmail.com>
2018-12-20 20:38:03 -0700

    pull out all 'LandR' functions
    this is a major change!
    
    https://github.com/PredictiveEcology/LandR@development

--- R/gis.R
+++ R/gis.R
@@ -19,32 +19,6 @@ createPrjFile <- function(shpFile,
   }
 }
 
-#' Do an arbitrary set of operations on a polygon
-#'
-#' @param poly A polygon object, or a character string identifying the shapefile
-#'             path to load, and clean.
-#' @param fn   A function identifying the type of cleaning to do.
-#' @param type If fn is not known, an character string can be specified to
-#'             identify which \code{fn} to use.
-#'             This MUST be a known type for this function.
-#' @param ...  Passed to \code{fn}
-#'
-#' @export
-#' @importFrom stats na.omit
-polygonClean <- function(poly, fn = NULL, type = NULL, ...) {
-  if (is.null(fn)) {
-    if (is.null(type)) {
-      stop("Either fn or type must be specified")
-    } else {
-      if (length(na.omit(pmatch(c("LandWeb", "tolko", "LP", "testing"), type))))
-        fn <- .cleanLandWebStudyArea
-      else
-        stop("Unknown type")
-    }
-  }
-  poly <- fn(poly, ...)
-}
-
 #' Faster version of \code{\link[raster]{factorValues}}
 #'
 #' Note there is an option to remove the NAs, which will make it MUCH faster,
@@ -52,10 +26,11 @@ polygonClean <- function(poly, fn = NULL, type = NULL, ...) {
 #'
 #' @inheritParams raster::factorValues
 #'
-#' @importFrom raster levels
 #' @param na.rm Logical. If \code{TRUE}, then the NAs will be removed, returning a possibly
 #'              shorter vector
 #' @export
+#' @importFrom raster levels
+#' @importFrom stats na.omit
 factorValues2 <- function(x, v, layer, att, append.names, na.rm = FALSE) {
   levs <- raster::levels(x)[[1]];
   if (isTRUE(na.rm))
