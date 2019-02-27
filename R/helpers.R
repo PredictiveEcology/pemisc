3c8d5163d50c5f7921ea47750c346206592333ed
Alex Chubaty <alex.chubaty@gmail.com>
2019-02-27 12:22:23 -0700

    writeSeedInfo() ==> writeRNGInfo(); add writeEventInfo()

--- R/helpers.R
+++ R/helpers.R
@@ -25,7 +25,27 @@ user <- function(username = NULL) {
   }
 }
 
-#' Write RNG seed info to file
+#' Write sim event info to file
+#'
+#' Useful for debugging.
+#'
+#' @param file Character specifying the filename (default \code{"events.txt"}).
+#' @param append  Logical indicating whether to append to the file (default \code{FALSE}).
+#'
+#' @return Nothing returned. Invoked for its side-effect of writing to file.
+#'
+#' @author Alex Chubaty
+#' @export
+#' @importFrom reproducible normPath
+#' @importFrom SpaDES.core current
+writeEventInfo <- function(sim, file = "events.txt", append = FALSE) {
+  f <- normPath(file)
+  curr <- current(sim)
+  cat(paste(curr$moduleName, curr$eventType, curr$eventTime), ":\n",
+      file = file, append = append)
+}
+
+#' Write RNG state info to file
 #'
 #' Useful for debugging and ensuring reproducibility.
 #'
@@ -37,7 +57,7 @@ user <- function(username = NULL) {
 #' @author Alex Chubaty
 #' @export
 #' @importFrom reproducible normPath
-writeSeedInfo <- function(file = "seed.txt", append = FALSE) {
+writeRNGInfo <- function(file = "seed.txt", append = FALSE) {
   fseed <- normPath(file)
   cat("\tStart of new RNG stream: ", file = fseed, append = append)
   ## NOTE: the first element of .Random.seed specifies the RNG type, so omit it
