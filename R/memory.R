7467431b3036c45232eaaf172f9aa977c1c337fb
Alex Chubaty <alex.chubaty@gmail.com>
2019-07-30 08:57:03 -0600

    clarify 'availableMemory' code and docs

--- R/memory.R
+++ R/memory.R
@@ -1,17 +1,17 @@
 #' Report the estimated amount of available memory in the OS
 #'
-#' This uses \code{system("free -lb", intern = TRUE))}, which will
-#' only work on Linux-alikes. On if the program 'free' is not installed
-#' on the system, this function returns \code{NULL}
+#' This reports the 'available' memory from a system \code{free} call.
+#' If \code{free} is not installed on the system (e.g., non-Linux), returns \code{NULL}.
+#'
+#' @return Numeric of class "object_size", so it can be reported in any units with format,
+#' e.g., \code{format(availableMemory(), unit = "GB")}.
 #'
 #' @export
-#' @return
-#' Returns a numeric, of class "object_size", so it can be reported
-#' in any units with format, e.g.,
-#' \code{format(availableMemory(), unit = "GB")}
+#' @seealso \code{man free} for description of available memory estimation.
 availableMemory <- function() {
-  aa <- try(system("free -lb", intern = TRUE), silent = TRUE)
+  aa <- try(system("free -b", intern = TRUE), silent = TRUE)
   availMem <- if (!is(aa, "try-error")) {
+    ## take the 'available' column from the 'Mem:' row of the table
     bb <- strsplit(aa[2], split = " ")
     as.numeric(bb[[1]][nzchar(bb[[1]])][7])
   } else {
