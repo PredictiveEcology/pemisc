653b938c722676bca8e26e40a0b80edd6e57654e
Alex Chubaty <alex.chubaty@gmail.com>
2019-09-13 15:09:58 -0600

    add macOS support to 'availableMemory()'
    sorry Windows users!

--- R/memory.R
+++ R/memory.R
@@ -1,7 +1,8 @@
 #' Report the estimated amount of available memory in the OS
 #'
-#' This reports the 'available' memory from a system \code{free} call.
-#' If \code{free} is not installed on the system (e.g., non-Linux), returns \code{NULL}.
+#' This reports the 'available' memory from a system \code{free} call on Linux,
+#' or a system \code{vm_stat} call on macOS.
+#' If neither is installed on the system (e.g., Windows), returns \code{NULL}.
 #'
 #' @return Numeric of class "object_size", so it can be reported in any units with format,
 #' e.g., \code{format(availableMemory(), unit = "GB")}.
@@ -9,20 +10,39 @@
 #' @export
 #' @seealso \code{man free} for description of available memory estimation.
 availableMemory <- function() {
-  aa <- try(system("free -b", intern = TRUE), silent = TRUE)
-  availMem <- if (!is(aa, "try-error")) {
-    ## take the 'available' column from the 'Mem:' row of the table
-    bb <- strsplit(aa[2], split = " ")
-    as.numeric(bb[[1]][nzchar(bb[[1]])][7])
+  free <- Sys.which("free")      ## Linux only
+  vmstat <- Sys.which("vm_stat") ## macOS
+
+  if (nzchar(free)) {
+    aa <- try(system(paste(free, "-b"), intern = TRUE), silent = TRUE)
+    availMem <- if (!is(aa, "try-error")) {
+      ## take the 'available' column from the 'Mem:' row of the table
+      bb <- strsplit(aa[2], split = " ")
+      as.numeric(bb[[1]][nzchar(bb[[1]])][7])
+    }
+  } else if (nzchar(vmstat)) {
+    aa <- try(system(vmstat, intern = TRUE), silent = TRUE)
+    availMem <- if (!is(aa, "try-error")) {
+      ## take the 'Pages free' row value times by the page size value
+      pageSize <- strsplit(aa[1], split = "page size of ")[[1]][2] %>%
+        strsplit(., split = " bytes)") %>%
+        `[[`(., 1) %>%
+        as.numeric()
+      bb <- strsplit(aa[2], split = " ")[[1]] %>%
+        unique()
+      pagesFree <- bb[which(!(bb %in% c("Pages", "free:", "")))] %>%
+        as.numeric()
+      pagesFree * pageSize ## in bytes
+    }
   } else {
-    NULL
+    availMem <- NULL
   }
 
   if (!is.null(availMem)) {
     class(availMem) <- "object_size"
   } else {
     warning("unable to determine available memory.",
-            " 'free' must be available on your system (typically Linux-only).")
+            " 'free' or 'vm_stat' must be available on your system.")
   }
   return(availMem)
 }
