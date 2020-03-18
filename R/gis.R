5e47aca1af2feb2642559a503da4f7c0de38dd7f
Alex Chubaty <alex.chubaty@gmail.com>
2020-03-18 11:49:18 -0600

    version 0.0.2.9000
    pull in `normalizeStack` from NWT & WB related projects

--- R/gis.R
+++ R/gis.R
@@ -91,3 +91,22 @@ setMethod("rasterToMatch", signature = "SpatialPolygonsDataFrame",
             rtm[is.na(rasterToMatch[])] <- NA
             rtm
 })
+
+#' Normalize each layer of a \code{RasterStack}
+#'
+#' Rescales the values of of each \code{RasterLayer} between \code{[0,1]}.
+#'
+#' @param x A \code{RasterStack} object.
+#'
+#' @author Tati Micheletti
+#' @export
+#' @importFrom amc rescale
+normalizeStack <- function(x) {
+  normalized <- lapply(names(x), function(layer) {
+    lay <- rescale(x[[layer]])
+    names(lay) <- layer
+    return(lay)
+  })
+  names(normalized) <- names(x)
+  return(normalized)
+}
