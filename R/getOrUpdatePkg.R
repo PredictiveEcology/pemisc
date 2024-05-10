72fad3109e60d27dc57d414620e50a806cf8034b
Eliot McIntire <eliotmcintire@gmail.com>
2024-05-10 14:29:23 -0700

    Vectorized getOrUpdatePkg.R

--- R/getOrUpdatePkg.R
+++ R/getOrUpdatePkg.R
@@ -1,6 +1,13 @@
 getOrUpdatePkg <- function(p, minVer = "0") {
-  if (!isFALSE(try(packageVersion(p) < minVer, silent = TRUE) )) {
-    repo <- c("predictiveecology.r-universe.dev", getOption("repos"))
-    install.packages(p, repos = repo)
+  p <- lapply(p, function(pp) {
+    if (!isFALSE(try(packageVersion(pp) < minVer, silent = TRUE) )) {
+      repo <- c("predictiveecology.r-universe.dev", getOption("repos"))
+      pp
+    } else {
+      NULL
+    }
   }
+  )
+  if (length(unlist(p)))
+    install.packages(unlist(p), repos = repo)
 }
