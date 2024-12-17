d7e653ceedcb0eae290b7b8385ba3cb41755508f
Eliot McIntire <eliotmcintire@gmail.com>
2024-12-17 07:46:17 -0800

    bugfix getOrUpdatePkg.R

--- R/getOrUpdatePkg.R
+++ R/getOrUpdatePkg.R
@@ -2,8 +2,8 @@ getOrUpdatePkg <- function(p, minVer = "0") {
   repo <- c("predictiveecology.r-universe.dev", getOption("repos"))
   if (length(p) != length(minVer)) stop("minVer must be as long as p")
   p <- Map(pp = p, mv = minVer, function(pp, mv) {
-    if (!isFALSE(try(packageVersion(pp) < mv, silent = TRUE) )) {
-      pp
+    if (!isFALSE(try(packageVersion(p) < mv, silent = TRUE) )) {
+      p
     } else {
       NULL
     }
